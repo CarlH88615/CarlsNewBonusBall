9afe29cf050c1307836d1eac50de0dbc7de96368
Carl Halliday <carlhalliday@Carls-MacBook-Air.local>
2026-01-06 16:03:35 +0000

    Explicitly request notification permission before push subscribe

--- sw.js
+++ sw.js
@@ -10,8 +10,8 @@ self.addEventListener('push', (event) => {
   const data = event.data ? event.data.json() : { title: 'Bonus Ball Update', body: 'New result available!' };
   const options = {
     body: data.body,
-    icon: 'https://cdn-icons-png.flaticon.com/512/3551/3551525.png',
-    badge: 'https://cdn-icons-png.flaticon.com/512/3551/3551525.png',
+    icon: '/icons/icon-192.png',
+badge: '/icons/icon-192.png',
     vibrate: [100, 50, 100],
     data: { url: '/' }
   };
